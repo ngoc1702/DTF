2846fc4dbca8efa59082cc9d144b3deb601e98de
ngoc <ngocplt2001@gmail.com>
2024-05-07 23:37:00 +0700

    home

---
+++ assets/js/ngoc_swiper.js
@@ -0,0 +1,51 @@
+var swiper = new Swiper(".mySwiper_mb", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper_doitac", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper_doitac2", {
+    slidesPerView: 2.5,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
